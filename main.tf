6e0bb681d577660cfd14a9bed8e21ec36b1de43d
Denys Naumenko <denaumenko@gmail.com>
2020-09-28 11:04:45 +0300

    Deploy one server to AWS

--- main.tf
+++ main.tf
@@ -5,7 +5,34 @@ provider "aws"{
 resource "aws_instance" "example" {
     ami = "ami-0dba2cb6798deb6d8"
     instance_type = "t2.micro"
-  tags = {    
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    
+    
+    user_data  =  "${file("webserver.sh")}"
+    
+    tags = {    
       Name = "terraform-example"  
       }
+
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"  
+  ingress {    
+      from_port   = var.server_port 
+      to_port     = var.server_port    
+      protocol    = "tcp"    
+      cidr_blocks = ["0.0.0.0/0"] 
+    }
+}
+
+variable "server_port" {  
+    description = "The port the server will use for HTTP requests"  
+    type        = number
+    default     = 8080
+}
+
+output "public_ip" {  
+    value       = aws_instance.example.public_ip  
+    description = "The public IP address of the web server"
+    }
